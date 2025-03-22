4472007eefb80f13abf06197266aefa614adff3b
Rohit Sunku <103047207+rgs8890@users.noreply.github.com>
2025-03-22 23:26:38 +0000

    Add files via upload

---
+++ foundationalc#/043-message-replace/Program.cs
@@ -0,0 +1,13 @@
+﻿string data = "12345John Smith          5000  3  ";
+string updatedData = data.Remove(5, 20); //Removes text between a string
+Console.WriteLine(updatedData);
+
+string message = "This--is--ex-amp-le--da-ta";
+
+message = message.Replace("--", " "); //Replaces -- a space
+message = message.Replace("-", "");
+
+Console.WriteLine(message);
+
+// Remove() method works like the Substring() method, except that it deletes the specified characters in the string
+// Replace() method swaps all instances of a string with a new string
---
+++ foundationalc#/044-void-method1/Program.cs
@@ -0,0 +1,15 @@
+﻿// void DisplayRandomNumbers(); -> Is how you declare a function that does not return anything
+
+void DisplayRandomNumbers()
+{
+    Random random = new Random();
+
+    for (int i = 0; i < 5; i++)
+    {
+        Console.Write($"{random.Next(1, 100)}");
+    }
+    Console.WriteLine();
+}
+
+Console.WriteLine("Generating random numbers:");
+DisplayRandomNumbers();
