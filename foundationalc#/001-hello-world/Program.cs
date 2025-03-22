1b09a62ea6048619e4c57921e6ab1f6cddd28d4e
Rohit Sunku <103047207+rgs8890@users.noreply.github.com>
2025-03-22 19:18:40 +0000

    Add files via upload

---
+++ foundationalc#/001-hello-world/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace TestProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Random dice = new Random();
+            int roll = dice.Next(1, 7);
+            Console.WriteLine(roll);
+
+            int result = dice.Next(1, 7);
+            Console.WriteLine(result);
+
+            Console.WriteLine(dice.Next(1, 7));
+            dice.Next(1, 7);
+
+            int number = 7;
+            string text = "seven";
+
+            Console.WriteLine(number);
+            Console.WriteLine();
+            Console.WriteLine(text);
+
+            Random dicex = new Random();
+            int rollx = dicex.Next();
+            int rollx2 = dicex.Next(101);
+            int rollx3 = dicex.Next(50, 101);
+
+            Console.WriteLine($"First roll: {rollx}");
+            Console.WriteLine($"Second roll: {rollx2}");
+            Console.WriteLine($"Third roll: {rollx3}");
+            
+            
+        }
+    }
+}
