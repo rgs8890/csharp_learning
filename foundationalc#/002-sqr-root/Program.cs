d68b54f5c7d53d474bd27968be6c92c58361d52b
Rohit Sunku <103047207+rgs8890@users.noreply.github.com>
2025-03-22 22:14:32 +0000

    Add files via upload

---
+++ foundationalc#/002-sqr-root/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Testproject2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("The square root of 16 is " + Math.Sqrt(16));
+
+            int firstValue = 500;
+            int secondValue = 1000;
+            int largerValue = Math.Max(firstValue, secondValue);
+
+            Console.WriteLine(largerValue);
+        }
+    }
+}
