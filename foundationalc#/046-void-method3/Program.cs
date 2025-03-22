b89e2d89ebc254c89fe888c8d7987b247453c532
Rohit Sunku <103047207+rgs8890@users.noreply.github.com>
2025-03-22 23:27:46 +0000

    Add files via upload

---
+++ foundationalc#/046-void-method3/Program.cs
@@ -0,0 +1,56 @@
+﻿string[] ipv4Input = {"107.31.1.5", "255.0.0.255", "555..0.555", "255...255"};
+string[] address;
+bool validLength = false;
+bool validZeroes = false;
+bool validRange = false;
+
+foreach (string ipv4 in ipv4Input)
+{
+    address = ipv4.Split(".", StringSplitOptions.RemoveEmptyEntries);
+
+    ValidLength();
+    ValidZeroes();
+    ValidRange();
+
+    if (validLength && validZeroes && validRange)
+    {
+        Console.WriteLine($"{ipv4} is a valid IPv4 address.");
+    }
+    else
+    {
+        Console.WriteLine($"{ipv4} is an invalid IPv4 address.");
+    }
+}
+
+void ValidLength()
+{
+    validLength = address.Length == 4;
+};
+
+void ValidZeroes()
+{
+    foreach (string number in address)
+    {
+        if (number.Length > 1 && number.StartsWith("0"))
+        {
+            validZeroes = false;
+        }
+    }
+
+    validZeroes = true;
+};
+
+void ValidRange()
+{
+    foreach (string number in address)
+    {
+        int value = int.Parse(number);
+        if (value < 0 || value > 255)
+        {
+            validRange = false;
+            return;
+        }
+    }
+    validRange = true;
+}
+
---
+++ foundationalc#/048-void-parameters/Program.cs
@@ -0,0 +1,103 @@
+﻿// Add parameters to methods
+CountTo(5);
+
+void CountTo(int max)
+{
+    for (int i = 0; i < max; i++)
+    {
+        Console.Write($"{i}, ");
+    }
+    Console.WriteLine();
+}
+
+int[] schedule = {800, 1200, 1600, 2000};
+
+void DisplayAdjustedTimes(int[] times, int currentGMT, int newGMT) 
+{
+    int diff = 0;
+    if (Math.Abs(newGMT) > 12 || Math.Abs(currentGMT) > 12)
+    {
+        Console.WriteLine("Invalid GMT");
+    }
+    else if (newGMT <= 0 && currentGMT <= 0 || newGMT >= 0 && currentGMT >= 0) 
+    {
+        diff = 100 * (Math.Abs(newGMT) - Math.Abs(currentGMT));
+    } 
+    else 
+    {
+        diff = 100 * (Math.Abs(newGMT) + Math.Abs(currentGMT));
+    }
+    for (int i = 0; i < times.Length; i++) 
+    {
+        int newTime = ((times[i] + diff)) % 2400;
+        Console.WriteLine($"{times[i]} -> {newTime}");
+    }
+}
+DisplayAdjustedTimes(schedule, 6, -6);
+
+//Recap
+// Information can be passed to methods in the form of parameters
+// Parameters are declared in the method signature
+// Multiple parameters are seperated by commas
+// Methods can accept variable or literal arguments
+
+
+// Variable Scope
+// Variables declared inside of a method are only accessible to that method.
+// Variables declared in top-level statements are accessible throughout the program.
+// Methods don't have access to variables defined within different methods.
+// Methods can call other methods.
+
+string[] students = {"Jenna", "Ayesha", "Carlos", "Viktor"};
+
+DisplayStudents(students);
+DisplayStudents(new string[] {"Robert","Vanya"});
+
+void DisplayStudents(string[] students) 
+{
+    foreach (string student in students) 
+    {
+        Console.Write($"{student}, ");
+    }
+    Console.WriteLine();
+}
+
+
+
+// Here the variable is declared inside the scope
+void PrintCircleArea(int radius)
+{
+    double pi = 3.14159;
+    double area = pi * (radius * radius);
+    Console.WriteLine($"Area = {area}");
+}
+PrintCircleArea(12);
+double pi = 3.14159;
+
+// In this case the variable is declared outside of the scope
+void PrintCircleAreax(int radius)
+{
+    double area = pi * (radius * radius);
+    Console.WriteLine($"Area = {area}");
+}
+PrintCircleAreax(12);
+
+// Here we can see the circumference of the circle which is printed
+void PrintCircleCircumference(int radius)
+{
+    double circumference = 2 * pi * radius;
+    Console.WriteLine($"Circumference = {circumference}");
+}
+
+
+//double circumference = 2 * pi * radius;
+void PrintCircleInfo(int radius) 
+{
+    Console.WriteLine($"Circle with radius {radius}");
+    PrintCircleArea(radius);
+    PrintCircleCircumference(radius);
+}
+
+PrintCircleInfo(12);
+PrintCircleInfo(24);
+
