f326a84120fe593d3686421595148e9e155ef18a
Rohit Sunku <103047207+rgs8890@users.noreply.github.com>
2025-03-22 22:50:58 +0000

    Add files via upload

---
+++ foundationalc#/016-numbers-1/Program.cs
@@ -0,0 +1,22 @@
+﻿int[] numbers = {4, 8, 15, 16, 23, 42};
+bool found = false;
+int total = 0;
+
+foreach (int number in numbers)
+{
+    total += number;
+
+    if (number == 42)
+        found = true;
+}
+
+if (found) 
+{
+    Console.WriteLine("Set contains 42.");
+}
+else
+{
+    Console.WriteLine("Set does not contain 42.");
+}
+
+Console.WriteLine($"Total: {total}");
---
+++ foundationalc#/017-switch-1/Program.cs
@@ -0,0 +1,61 @@
+﻿// How to add the branching logic to one that matches one variable or expression against many possible values
+// Switch-Case Statement construct to match the variable or expression against other outcomes, using an if-elseif-else construct
+
+using System;
+
+string fruit = "apple";
+
+switch (fruit)
+{
+    case "apple":
+        Console.WriteLine("Apple is a fruit");
+        break;
+    case "banana":
+        Console.WriteLine("Banana is a fruit");
+        break;
+    case "cherry":
+        Console.WriteLine("Orange is a fruit");
+        break;
+    default:
+        Console.WriteLine("Unknown fruit");
+        break;
+}
+// Each switch is defined in a case pattern
+
+int employeeLevel = 200;
+string employeeName = "John Smith";
+
+string title = "";
+
+switch (employeeLevel)
+{
+    case 100:
+        title = "Junior Associate";
+        break;
+    case 200:
+        title = "Associate";
+        break;
+    case 300:
+        title = "Senior Associate";
+        break;
+    case 400:
+        title = "Manager";
+        break;
+    case 500:
+        title = "Senior Manager";
+        break;
+    case 600:
+        title = "Director";
+        break;
+    default:
+        title = "Unknown";
+        break;
+}
+
+Console.WriteLine($"{employeeName}, {title}.");
+
+// Switch statement should be used with one value that has many possible matches, each match needing to have a branch in the coding logic
+// Single Switch Statement code logic can be matched using one or more labels defined by the case keyword
+// An optional default label and switch section can be used when no other case labels matched
+
+
