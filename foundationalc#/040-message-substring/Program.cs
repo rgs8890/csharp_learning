9252876ab3c66e66529a5110a874c7f62621fadf
Rohit Sunku <103047207+rgs8890@users.noreply.github.com>
2025-03-22 23:25:45 +0000

    Add files via upload

---
+++ foundationalc#/040-message-substring/Program.cs
@@ -0,0 +1,14 @@
+﻿string message = "(What if) there are (more than) one (set of parantheses)?";
+
+while (true)
+{
+    int openingPosition = message.IndexOf('(');
+    if (openingPosition == -1) break;
+
+    openingPosition += 1;
+    int closingPosition = message.IndexOf(')');
+    int length = closingPosition - openingPosition;
+    Console.WriteLine(message.Substring(openingPosition, length));
+
+    message = message.Substring(closingPosition + 1);
+}
---
+++ foundationalc#/041-message-indexofany/Program.cs
@@ -0,0 +1,18 @@
+﻿string message = "Hello, world!";
+char[] charsToFind = { 'a', 'e', 'i'};
+
+int index = message.IndexOfAny(charsToFind);
+
+Console.WriteLine($"Found '{message[index]} in '{message}' at index: {index}.");
+
+string message2 = "Help (find) the {opening symbols}";
+Console.WriteLine($"Searching THIS Message: {message2}");
+
+char[] openSymbols = {'[', '{', '('};
+int startPosition = 5;
+int openingPosition = message2.IndexOfAny(openSymbols);
+Console.WriteLine($"Found WITHOUT using startPosition: {message2.Substring(openingPosition)}");
+
+openingPosition = message2.IndexOfAny(openSymbols, startPosition);
+Console.WriteLine($"Found WITH using startPosition {startPosition}: {message2.Substring(openingPosition)}");
+
