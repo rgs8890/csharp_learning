ac5f17781446c51a09901d6397984dedde3e0dfc
Rohit Sunku <103047207+rgs8890@users.noreply.github.com>
2025-03-22 23:45:53 +0000

    Add files via upload

---
+++ foundationalc#/053-ispalindrome/Program.cs
@@ -0,0 +1,30 @@
+﻿//String Methods
+string input = "snake";
+
+string ReverseWord(string word)
+{
+    string result = "";
+    for (int i = word.Length - 1; i >= 0; i--)
+    {
+        result += word[i];
+    }
+    return result;
+}
+
+Console.WriteLine(input);
+Console.WriteLine(ReverseWord(input));
+
+string input2 = "there are snakes at the zoo";
+
+string ReverseSentence(string sentence)
+{
+    string result = "";
+    string[] words = sentence.Split(" ");
+    foreach (string word in words)
+    {
+        result += ReverseWord(word) + " ";
+    }
+    return result.Trim();
+}
+
+Console.WriteLine(ReverseSentence(input2));
