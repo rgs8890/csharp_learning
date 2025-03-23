fce3b7492e56ad87b9d6642437e69643f723206f
Rohit Sunku <103047207+rgs8890@users.noreply.github.com>
2025-03-23 15:11:24 +0000

    Add files via upload

---
+++ niit-object-orientated-programming/003_enter_number/MyProgram2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyProgram2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, y, z;
+            Console.Write("Enter a number : ");
+            x = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter a number : ");
+            y = Convert.ToInt32(Console.ReadLine());
+            z = x + y;
+            Console.WriteLine("Addition of " + x + " and " + y + " is " + z);
+            Console.WriteLine("Addition of {0} and {1} is {2}", x, y, z);
+            Console.ReadKey();
+        }
+    }
+}
